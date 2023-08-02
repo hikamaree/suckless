1de96ff04200a002fd859ede8fd8bc4b08b60625
hikamaree <marko.r463@gmail.com>
2023-08-02 13:12:57 +0200

    update

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx      = 2;
-static const unsigned int gappx         = 16;
+static const unsigned int gappx         = 10;
 static const unsigned int snap          = 32;
 static const int showbar                = 1;
 static const int topbar                 = 1;
@@ -68,83 +68,72 @@ static char dmenumon[2] = "0";
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* scratchpad */
-typedef struct {
-    const char *name;
-    const void *cmd;
-} Sp;
-
-const char *spcmd1[] = {"st", "-n", "spst", "-g", "120x30", NULL };
-const char *spcmd2[] = {"st", "-n", "splf", "-g", "120x30", "-e", "lf", NULL };
-const char *spcmd3[] = {"st", "-n", "spmpd", "-g", "120x30", "-e", "ncmpcpp", NULL };
-
-static Sp scratchpads[] = {
-    /* name          cmd  */
-    {"spst",      spcmd1},
-    {"splf",    spcmd2},
-    {"spmpd",   spcmd3},
+/* scratchpads */
+static void *scratchpads[][10] = {
+    {"st", "-n", "spst", "-g", "120x30", NULL },
+    {"st", "-n", "splf", "-g", "120x30", "-e", "lf", NULL },
+    {"st", "-n", "spmpd", "-g", "120x30", "-e", "ncmpcpp", NULL },
 };
 
-
 /* commands */
-static const char *dmenucmd[]       = { "dmenu_run", NULL };
-static const char *discord[]        = { "discord", NULL };
 static const char *termcmd[]        = { "st", NULL };
-static const char *browser[]        = { "firefox", NULL };
+static const char *dmenucmd[]       = { "dmenu_run", NULL };
 static const char *volumeup[]       = { "volume", "up", NULL };
 static const char *volumedown[]     = { "volume", "down", NULL };
 static const char *mute[]           = { "volume", "mute", NULL };
 static const char *brightnessup[]   = { "brightness", "up", NULL };
 static const char *brightnessdown[] = { "brightness", "down", NULL };
+static const char *browser[]        = { "firefox", NULL };
+static const char *discord[]        = { "discord", NULL };
 
 /* shortcuts */
 static const Key keys[] = {
-	/* modifier         key         function        argument */
-	{ MODKEY|ShiftMask, XK_Return,  spawn,          {.v = termcmd } },
-	{ MODKEY,           XK_x,       togglescratch,  {.ui = 0 } },
-	{ MODKEY,           XK_c,       togglescratch,  {.ui = 1 } },
-	{ MODKEY,           XK_v,	    togglescratch,  {.ui = 2 } },
-	{ 0,                XK_Super_L, spawn,    	    {.v = dmenucmd } },
-	{ MODKEY,           XK_b,       spawn,          {.v = browser } },
-	{ MODKEY,           XK_n,       spawn,          {.v = discord } },
-	{ MODKEY,           XK_q,       togglebar,      {0} },
-	{ 0,                Mute,     	spawn,          {.v = mute} },
-	{ 0,                VolDown,    spawn,          {.v = volumedown} },
-	{ 0,                VolUp,      spawn,          {.v = volumeup} },
-	{ 0,                BrtDown,    spawn,          {.v = brightnessdown} },
-	{ 0,                BrtUp,      spawn,          {.v = brightnessup} },
-	{ MODKEY,           XK_j,       focusstack,     {.i = +1 } },
-	{ MODKEY,           XK_k,       focusstack,     {.i = -1 } },
-	{ MODKEY,           XK_i,       incnmaster,     {.i = +1 } },
-	{ MODKEY,           XK_d,       incnmaster,     {.i = -1 } },
-	{ MODKEY,           XK_h,       setmfact,       {.f = -0.05} },
-	{ MODKEY,           XK_l,       setmfact,       {.f = +0.05} },
-	{ MODKEY,           XK_z,       zoom,           {0} },
-	{ MODKEY,           XK_Tab,     view,           {0} },
-	{ MODKEY|ShiftMask, XK_c,       killclient,     {0} },
-	{ MODKEY,           XK_t,       setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,           XK_f,       setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,           XK_space,   setlayout,      {0} },
-	{ MODKEY,           XK_0,       view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask, XK_0,       tag,            {.ui = ~0 } },
-	{ MODKEY,           XK_comma,   focusmon,       {.i = -1 } },
-	{ MODKEY,           XK_period,  focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask, XK_comma,   tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask, XK_period,  tagmon,         {.i = +1 } },
-	TAGKEYS(            XK_1,                      0)
-	TAGKEYS(            XK_2,                      1)
-	TAGKEYS(            XK_3,                      2)
-	TAGKEYS(            XK_4,                      3)
-	TAGKEYS(            XK_5,                      4)
-	TAGKEYS(            XK_6,                      5)
-	TAGKEYS(            XK_7,                      6)
-	TAGKEYS(            XK_8,                      7)
-	TAGKEYS(            XK_9,                      8)
-	{ MODKEY|ShiftMask, XK_q,      quit,           {0} },
-	{ 0,                PrtSc,     spawn,
+	/* modifier         key             function        argument */
+	{ MODKEY|ShiftMask, XK_Return,      spawn,          {.v = termcmd } },
+	{ MODKEY,           XK_x,           togglescratch,  {.ui = 0 } },
+	{ MODKEY,           XK_c,           togglescratch,  {.ui = 1 } },
+	{ MODKEY,           XK_v,	        togglescratch,  {.ui = 2 } },
+	{ 0,                XK_Super_L,     spawn,    	    {.v = dmenucmd } },
+	{ MODKEY,           XK_b,           spawn,          {.v = browser } },
+	{ MODKEY,           XK_n,           spawn,          {.v = discord } },
+	{ MODKEY,           XK_q,           togglebar,      {0} },
+	{ 0,                Mute,     	    spawn,          {.v = mute} },
+	{ 0,                VolDown,        spawn,          {.v = volumedown} },
+	{ 0,                VolUp,          spawn,          {.v = volumeup} },
+	{ 0,                BrtDown,        spawn,          {.v = brightnessdown} },
+	{ 0,                BrtUp,          spawn,          {.v = brightnessup} },
+	{ MODKEY,           XK_j,           focusstack,     {.i = +1 } },
+	{ MODKEY,           XK_k,           focusstack,     {.i = -1 } },
+	{ MODKEY,           XK_i,           incnmaster,     {.i = +1 } },
+	{ MODKEY,           XK_d,           incnmaster,     {.i = -1 } },
+	{ MODKEY,           XK_h,           setmfact,       {.f = -0.05} },
+	{ MODKEY,           XK_l,           setmfact,       {.f = +0.05} },
+	{ MODKEY,           XK_z,           zoom,           {0} },
+	{ MODKEY,           XK_Tab,         view,           {0} },
+	{ MODKEY|ShiftMask, XK_c,           killclient,     {0} },
+	{ MODKEY,           XK_t,           setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,           XK_f,           setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,           XK_space,       setlayout,      {0} },
+	{ MODKEY,           XK_0,           view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask, XK_0,           tag,            {.ui = ~0 } },
+	{ MODKEY,           XK_comma,       focusmon,       {.i = -1 } },
+	{ MODKEY,           XK_period,      focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask, XK_comma,       tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask, XK_period,      tagmon,         {.i = +1 } },
+	TAGKEYS(            XK_1,           0)
+	TAGKEYS(            XK_2,           1)
+	TAGKEYS(            XK_3,           2)
+	TAGKEYS(            XK_4,           3)
+	TAGKEYS(            XK_5,           4)
+	TAGKEYS(            XK_6,           5)
+	TAGKEYS(            XK_7,           6)
+	TAGKEYS(            XK_8,           7)
+	TAGKEYS(            XK_9,           8)
+	{ MODKEY|ShiftMask, XK_q,           quit,           {0} },
+	{ 0,                PrtSc,          spawn,
         SHCMD("/usr/bin/maim -su | xclip -selection clipboard -t image/png; xclip -out -selection clipboard > ~/Pictures/screenshots/$(date '+%b%d_%H%M%S').png")
     },
-	{ ShiftMask,        PrtSc,     spawn,
+	{ ShiftMask,        PrtSc,          spawn,
         SHCMD("/usr/bin/maim -u | xclip -selection clipboard -t image/png; xclip -out -selection clipboard > ~/Pictures/screenshots/$(date '+%b%d_%H%M%S').png")
     },
 };
--- dwm/dwm.c
+++ dwm/dwm.c
@@ -67,8 +67,8 @@ enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
        NetWMFullscreen, NetActiveWindow, NetWMWindowType,
        NetWMWindowTypeDialog, NetClientList, NetLast }; /* EWMH atoms */
 enum { WMProtocols, WMDelete, WMState, WMTakeFocus, WMLast }; /* default atoms */
-enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
-       ClkClientWin, ClkRootWin, ClkLast }; /* clicks */
+enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkClientWin,
+       ClkRootWin, ClkLast }; /* clicks */
 
 typedef union {
 	int i;
@@ -471,10 +471,8 @@ buttonpress(XEvent *e)
 			arg.ui = 1 << i;
 		} else if (ev->x < x + TEXTW(selmon->ltsymbol))
 			click = ClkLtSymbol;
-		else if (ev->x > selmon->ww - (int)TEXTW(stext))
-			click = ClkStatusText;
 		else
-			click = ClkWinTitle;
+            click = ClkStatusText;
 	} else if ((c = wintoclient(ev->window))) {
 		focus(c);
 		restack(selmon);
@@ -747,7 +745,6 @@ void
 drawbar(Monitor *m)
 {
 	int x, w, tw = 0;
-	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
@@ -773,7 +770,7 @@ drawbar(Monitor *m)
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i)
-			drw_rect(drw, x + boxw + 2, 0, w - ( 2 * boxw - 2), boxw - 2,
+			drw_rect(drw, x + boxw + 2, 0, w - 2 * boxw, boxw - 2,
 					m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
 					urg & 1 << i);
 		x += w;
@@ -783,15 +780,8 @@ drawbar(Monitor *m)
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - x) > bh) {
-		if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
-			if (m->sel->isfloating)
-				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
-		} else {
-			drw_setscheme(drw, scheme[SchemeNorm]);
-			drw_rect(drw, x, 0, w, bh, 1, 1);
-		}
+		drw_setscheme(drw, scheme[SchemeNorm]);
+		drw_rect(drw, x, 0, w, bh, 1, 1);
 	}
 	drw_map(drw, m->barwin, 0, 0, m->ww, bh);
 }
@@ -1276,11 +1266,8 @@ propertynotify(XEvent *e)
 			drawbars();
 			break;
 		}
-		if (ev->atom == XA_WM_NAME || ev->atom == netatom[NetWMName]) {
+		if (ev->atom == XA_WM_NAME || ev->atom == netatom[NetWMName])
 			updatetitle(c);
-			if (c == c->mon->sel)
-				drawbar(c->mon);
-		}
 		if (ev->atom == netatom[NetWMWindowType])
 			updatewindowtype(c);
 	}
@@ -1767,7 +1754,7 @@ togglescratch(const Arg *arg)
 	Client *c;
 	unsigned int found = 0;
 	unsigned int scratchtag = SPTAG(arg->ui);
-	Arg sparg = {.v = scratchpads[arg->ui].cmd};
+	Arg sparg = {.v = scratchpads[arg->ui]};
 
 	for (c = selmon->clients; c && !(found = c->tags & scratchtag); c = c->next);
 	if (found) {
